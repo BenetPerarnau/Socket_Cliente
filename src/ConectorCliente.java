d964a153af2e600eb9bbde232e139bcfd0535f84
BenetPerarnau <benetperarnau@hotmail.com>
2014-10-12 17:54:42 +0200

    millora

--- src/ConectorCliente.java
+++ src/ConectorCliente.java
@@ -19,20 +19,31 @@ public class ConectorCliente {
 			cliente=new Socket(ip,puerto);
 			entrada=new BufferedReader (new InputStreamReader(cliente.getInputStream()));
 			teclado=new BufferedReader (new InputStreamReader(System.in));
-			
-			String input, msg;
+			System.out.println("Conectat al servidor (127.0.0.1:9000)");
+			//salida=new PrintStream(cliente.getOutputStream());
+			String input="", msg="";
 			do{
-				
-			input=teclado.readLine();
-			salida=new PrintStream(cliente.getOutputStream());
-			salida.println(input);
-			msg=entrada.readLine();
-			System.out.println(msg);
+				if(!msg.equalsIgnoreCase("exit")){
+					System.out.print("Client => ");	
+					input=teclado.readLine();
+					
+					
+						salida=new PrintStream(cliente.getOutputStream());
+						salida.println(input);
+					if(!input.equalsIgnoreCase("exit")){
+						msg=entrada.readLine();
+						System.out.println(msg);
+					}else{
+						System.out.println("Has desconectat la sesio");
+					}
+				}else{
+					System.out.println("El servidor a tancat la sesio");
+				}
 			
 			}while(!input.equalsIgnoreCase("exit") && !msg.equalsIgnoreCase("exit"));
 			
 		}catch(Exception e){
-			System.out.println("error");
+			System.out.println("El servidor no esta operatiu");
 		}
 	}
 	
